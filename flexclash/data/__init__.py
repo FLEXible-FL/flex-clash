85fc59455eca2331703ac45bd9ff3777408afe12
xehartnort <xehartnort@gmail.com>
2023-07-06 17:24:16 +0200

    add gitignore and imports

--- flexclash/data/__init__.py
+++ flexclash/data/__init__.py
@@ -1,1 +1,5 @@
-from poisoning_decorators import backdoor_data
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from flexclash.data.poisoning_decorators import backdoor_data
