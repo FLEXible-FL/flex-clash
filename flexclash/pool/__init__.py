cfc031be98acb24ef375d194a9fbf4143e6e270d
xehartnort <xehartnort@gmail.com>
2023-07-07 11:11:33 +0200

    fix .gitignore and add model poisoning features

--- flexclash/pool/__init__.py
+++ flexclash/pool/__init__.py
@@ -3,3 +3,4 @@ from __future__ import print_function
 from __future__ import absolute_import
 
 from flexclash.pool.defences import median, trimmed_mean, multikrum, bulyan
+from flexclash.pool.poisoning_decorators import model_poisoner
