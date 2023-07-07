f7258705573080836fb1357079258a1f2fa17e30
rbnuria <rbnuria6@gmail.com>
2023-07-07 10:01:48 +0200

    minor changes in backdoor attacks notebooks

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="flex-clash",
+    name="flexclash",
     version="0.0.1",
     author="Nuria Rodríguez Barroso",
     keywords="adversarial attacks defences FL federated-learning flexible",
