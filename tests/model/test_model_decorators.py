4d9d1ac0ff24d1dabc966e14c5cca8fe2c42dc70
xehartnort <xehartnort@gmail.com>
2023-10-04 17:46:48 +0200

    Add and update tests to work with the latest version of FLEXible

--- tests/model/test_model_decorators.py
+++ tests/model/test_model_decorators.py
@@ -24,7 +24,7 @@ class TestModelPoisoningDecorators(unittest.TestCase):
     @pytest.fixture(autouse=True)
     def _fixture_iris_dataset(self):
         iris = load_iris()
-        c_iris = Dataset(X_data=iris.data, y_data=iris.target)
+        c_iris = Dataset.from_numpy(iris.data, iris.target)
         self.f_iris = FedDataDistribution.iid_distribution(c_iris, n_clients=5)
 
     def test_decorators(self):
