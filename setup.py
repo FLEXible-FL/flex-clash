ed4f2912f5f4f3138d3ff181564f7942a4d2f4b9
Mario García Márquez <mariogarciamarq@gmail.com>
2024-10-22 11:31:49 +0200

    bump to 0.0.3 due to bug fix

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="flexclash",
-    version="0.0.2",
+    version="0.0.3",
     author="Nuria Rodríguez Barroso",
     keywords="adversarial attacks defences FL federated-learning flexible",
     long_description=open("README.md", "r", encoding="utf-8").read(),
